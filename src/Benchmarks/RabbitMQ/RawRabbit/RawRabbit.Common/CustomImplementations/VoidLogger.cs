fecb9a91a54e7e660c8c418bd837a9c4ab24531f
par.dahlman <par.dahlman@valtech.se>
2016-03-14 19:41:32 +0100

    Add benchmark solution for RawRabbit
    With similar setup as the other benchmark projects

---
+++ src/Benchmarks/RabbitMQ/RawRabbit/RawRabbit.Common/CustomImplementations/VoidLogger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using RawRabbit.Logging;
+
+namespace RawRabbit.Common.CustomImplementations
+{
+	public class VoidLogger : ILogger
+	{
+		public void LogDebug(string format, params object[] args)
+		{
+		}
+
+		public void LogInformation(string format, params object[] args)
+		{
+		}
+
+		public void LogWarning(string format, params object[] args)
+		{
+		}
+
+		public void LogError(string message, Exception exception)
+		{
+		}
+	}
+}
